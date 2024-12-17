53c2dfee3571a5c3add743f2aa8cb735d9f5c2bc
Lukman <103345757+LukmanHakim2204@users.noreply.github.com>
2024-12-17 16:23:35 +0700

    Detail Listing Datepicker

--- src/app/(root)/listing/[id]/page.tsx
+++ src/app/(root)/listing/[id]/page.tsx
@@ -156,7 +156,9 @@ function Detail({ params }: { params: { id: string } }) {
           <Map />
           <CustomerReviews />
         </div>
-        <BookingSection id={params?.id} />
+        {listing && (
+          <BookingSection id={params?.id} price={listing?.price_per_day} />
+        )}
       </section>
 
       <ListingShowcase
